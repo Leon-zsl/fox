eb9ed609d1aeff16190a8a73929ca754982fbf86
Leon-Zsl <slzhang1983@gmail.com>
2018-09-03 20:21:41 +0800

    add some common define

--- fox.h
+++ fox.h
@@ -9,7 +9,7 @@
 
 #define FOX_VERSION "0.0.1"
 
-#define log_error(fmt, ...) {			                       \
+#define log_error(fmt, ...) {			                        \
 	char errmsg[1024];											\
 	sprintf(errmsg, fmt, ##__VA_ARGS__);						\
 	fprintf(stdout, "[ERROR]FILE:%s LINE:%d %s\n", __FILE__, __LINE__, errmsg); \
@@ -26,5 +26,23 @@
 	sprintf(errmsg, fmt, ##__VA_ARGS__);						\
 	fprintf(stdout, "[INFO]FILE:%s LINE:%d %s\n", __FILE__, __LINE__, errmsg); \
 }
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+typedef char int8;
+typedef short int16;
+typedef int int32;
+typedef long long int64;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+typedef unsigned char byte;
+typedef unsigned short word;
+typedef unsigned int dword;
 
 #endif
